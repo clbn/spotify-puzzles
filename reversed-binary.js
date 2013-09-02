01e6cbded22546b331a3c432168115ef5ac11f93
Alex Olshansky <i@creagenics.com>
2013-09-02 22:11:47 +0400

    Reversed Binary: initial script

---
+++ reversed-binary.js
@@ -0,0 +1,14 @@
+﻿var dec = 47;
+var bin = dec.toString(2).split('');
+
+for (var i=0; i<bin.length/2; i++) {
+  if (bin[i] !== bin[bin.length-i-1]) {
+    // Swapping 0 and 1
+    bin[i] = [1, 0][ bin[i] ];
+    bin[bin.length-i-1] = [1, 0][ bin[bin.length-i-1] ];
+  }
+}
+
+dec = parseInt(bin.join(''), 2);
+
+console.log(dec);
