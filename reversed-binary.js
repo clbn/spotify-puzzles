67c64a2d00f2ced353993f9c3132033b4db2f74f
Alex Olshansky <i@creagenics.com>
2013-09-02 22:35:20 +0400

    Reversed Binary: stdin/stdout

--- reversed-binary.js
+++ reversed-binary.js
@@ -1,14 +1,25 @@
-﻿var dec = 22;
-var bin = dec.toString(2).split('');
+﻿var readline = require('readline');
 
-for (var i=0; i<=bin.length/2-1; i++) {
-  if (bin[i] !== bin[bin.length-i-1]) {
-    // Swapping 0 and 1
-    bin[i] = [1, 0][ bin[i] ];
-    bin[bin.length-i-1] = [1, 0][ bin[bin.length-i-1] ];
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function (line) {
+  rl.write(getReversedBinary(parseInt(line, 10)).toString());
+  rl.close();
+});
+
+var getReversedBinary = function(dec) {
+  var bin = dec.toString(2).split('');
+
+  for (var i=0; i<=bin.length/2-1; i++) {
+    if (bin[i] !== bin[bin.length-i-1]) {
+      // Swapping 0 and 1
+      bin[i] = [1, 0][ bin[i] ];
+      bin[bin.length-i-1] = [1, 0][ bin[bin.length-i-1] ];
+    }
   }
-}
 
-dec = parseInt(bin.join(''), 2);
-
-console.log(dec);
+  return parseInt(bin.join(''), 2);
+};
