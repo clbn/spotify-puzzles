3219fa62cf6377ef870c78384db7fc1f2b23eb24
Alex Olshansky <i@creagenics.com>
2013-09-03 13:31:35 +0400

    Zipf's Song: stdin/stdout

--- zipf-song.js
+++ zipf-song.js
@@ -1,22 +1,38 @@
-var input = [
- [15, 3],
- [197812, "re_hash"],
- [78906, "5_4"],
- [189518, "tomorrow_comes_today"],
- [39453, "new_genious"],
- [210492, "clint_eastwood"],
- [26302, "man_research"],
- [22544, "punk"],
- [19727, "sound_check"],
- [17535, "double_bass"],
- [18782, "rock_the_house"],
- [198189, "19_2000"],
- [13151, "latin_simone"],
- [12139, "starshine"],
- [11272, "slow_country"],
- [10521, "m1_a1"]
-];
+var input = '';
+var listNumber = 0;
+var selectionNumber = 0;
 
+// Reading input
+process.stdin.on('data', function(chunk) {
+  input += chunk;
+  var lines = input.trim().split('\n');
+  var match;
+
+  if (match = lines[0].match(/^(\d+)\s+(\d+)\s*/)) {
+    listNumber = match[1];
+    selectionNumber = match[2];
+  } else {
+    input = '';
+    return;
+  }
+
+  if (listNumber && lines.length > listNumber) {
+    var songs = [];
+    var i;
+    for (i=1; i<lines.length; i++) {
+      if (match = lines[i].match(/^(\d+)\s+([0-9a-z_]+)\s*$/)) {
+        songs.push([ i, match[1], match[2] ]);
+      }
+    }
+    var selectedSongs = getBestSongs(songs, selectionNumber);
+    for (i=0; i<selectedSongs.length; i++) {
+      process.stdout.write(selectedSongs[i][2] + '\n');
+    }
+    process.exit();
+  }
+});
+
+// Calculating quality of songs
 var getBestSongs = function(songs, selectionCount) {
   var i;
   for (i=0; i<listNumber; i++) {
@@ -29,18 +45,3 @@ var getBestSongs = function(songs, selectionCount) {
   });
   return songs.slice(0, selectionCount);
 };
-
-var listNumber = input[0][0];
-var selectionNumber = input[0][1];
-var songs = [];
-var i;
-
-for (i=1; i<input.length; i++) {
-  songs.push([ i, input[i][0], input[i][1] ]);
-}
-
-var selectedSongs = getBestSongs(songs, selectionNumber);
-
-for (i=0; i<selectedSongs.length; i++) {
-  console.log(selectedSongs[i][2]);
-}
